65152e3270fcb317e93fb40b2928a60f52cda4f9
nastya-gamza <nastyagamza2000@gmail.com>
2023-07-02 16:50:03 +0300

    added scroll-up btn and intro slider

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const headerBurger = document.querySelector(".header__burger");
 const nav = document.querySelector(".nav");
+const scrollUpBtn = document.querySelector(".scroll-up");
+const about = document.querySelector("#about");
 const tabs = Array.from(document.querySelectorAll('.tab'));
 
 
@@ -36,3 +38,50 @@ function tabToggle(e) {
         currentContent.style.maxHeight = 0;
     }
 }
+
+//INTRO
+
+const introBg = document.querySelector('.intro');
+const introSlide = document.querySelectorAll('.intro__slide');
+let counter = 1;
+
+setInterval(() => {
+    if(counter === introSlide.length) {
+        counter = 1;
+        switchSlide();
+    } else {
+        counter++;
+        switchSlide();
+    }
+}, 3000)
+
+function switchSlide() {
+    introBg.style.backgroundImage = `url(../img/main/bg-intro-${counter}.jpg)`;
+    introBg.classList.add('intro--active');
+
+    introSlide.forEach((slide, index) => {
+        if((index + 1) === counter) {
+            slide.classList.add('intro__slide--active');
+        } else {
+            slide.classList.remove('intro__slide--active');
+        }
+    })
+}
+
+//SCROLL UP
+
+window.addEventListener("scroll", () => {
+    btnVisibility();
+});
+
+const btnVisibility = () => {
+    if (about.getBoundingClientRect().top <= 0) {
+        scrollUpBtn.style.visibility = "visible";
+    } else {
+        scrollUpBtn.style.visibility = "hidden";
+    }
+};
+
+scrollUpBtn.addEventListener('click', () => {
+    window.scrollTo(scrollX, 0);
+})
